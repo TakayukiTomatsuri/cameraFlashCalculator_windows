be98a96471f7d8848c57d2d93f7351babfac082b
tt_vs <tomataka375@gmail.com>
2019-08-01 04:49:14 +0900

    refactor all GUI parts names.

--- cameraFlashCalculator_form/MyForm.h
+++ cameraFlashCalculator_form/MyForm.h
@@ -37,16 +37,16 @@ namespace cameraFlashCalculatorform {
 				delete components;
 			}
 		}
-	private: System::Windows::Forms::Button^ button1;
-	private: System::Windows::Forms::TextBox^ textBox1;
-	private: System::Windows::Forms::TextBox^ textBox2;
-	private: System::Windows::Forms::TextBox^ textBox3;
+	private: System::Windows::Forms::Button^ button_calc;
+	private: System::Windows::Forms::TextBox^ textBox_dist;
+	private: System::Windows::Forms::TextBox^ textBox_iso;
+	private: System::Windows::Forms::TextBox^ textBox_gn;
 	private: System::Windows::Forms::Label^ label1;
-	private: System::Windows::Forms::Label^ label2;
-	private: System::Windows::Forms::Label^ label3;
-	private: System::Windows::Forms::TextBox^ textBox4;
-	private: System::Windows::Forms::Label^ label4;
-	private: System::Windows::Forms::Label^ label5;
+	private: System::Windows::Forms::Label^ label_dist;
+	private: System::Windows::Forms::Label^ label_iso;
+	private: System::Windows::Forms::TextBox^ textBox_fVal;
+	private: System::Windows::Forms::Label^ label_fVal;
+	private: System::Windows::Forms::Label^ label_gn;
 	protected:
 
 	private:
@@ -62,52 +62,52 @@ namespace cameraFlashCalculatorform {
 		/// </summary>
 		void InitializeComponent(void)
 		{
-			this->button1 = (gcnew System::Windows::Forms::Button());
-			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
-			this->textBox2 = (gcnew System::Windows::Forms::TextBox());
-			this->textBox3 = (gcnew System::Windows::Forms::TextBox());
+			this->button_calc = (gcnew System::Windows::Forms::Button());
+			this->textBox_dist = (gcnew System::Windows::Forms::TextBox());
+			this->textBox_iso = (gcnew System::Windows::Forms::TextBox());
+			this->textBox_gn = (gcnew System::Windows::Forms::TextBox());
 			this->label1 = (gcnew System::Windows::Forms::Label());
-			this->label2 = (gcnew System::Windows::Forms::Label());
-			this->label3 = (gcnew System::Windows::Forms::Label());
-			this->textBox4 = (gcnew System::Windows::Forms::TextBox());
-			this->label4 = (gcnew System::Windows::Forms::Label());
-			this->label5 = (gcnew System::Windows::Forms::Label());
+			this->label_dist = (gcnew System::Windows::Forms::Label());
+			this->label_iso = (gcnew System::Windows::Forms::Label());
+			this->textBox_fVal = (gcnew System::Windows::Forms::TextBox());
+			this->label_fVal = (gcnew System::Windows::Forms::Label());
+			this->label_gn = (gcnew System::Windows::Forms::Label());
 			this->SuspendLayout();
 			// 
-			// button1
+			// button_calc
 			// 
-			this->button1->Location = System::Drawing::Point(108, 138);
-			this->button1->Name = L"button1";
-			this->button1->Size = System::Drawing::Size(75, 23);
-			this->button1->TabIndex = 0;
-			this->button1->Text = L"Calculate !";
-			this->button1->UseVisualStyleBackColor = true;
-			this->button1->Click += gcnew System::EventHandler(this, &MyForm::Button1_Click);
+			this->button_calc->Location = System::Drawing::Point(108, 138);
+			this->button_calc->Name = L"button_calc";
+			this->button_calc->Size = System::Drawing::Size(75, 23);
+			this->button_calc->TabIndex = 0;
+			this->button_calc->Text = L"Calculate !";
+			this->button_calc->UseVisualStyleBackColor = true;
+			this->button_calc->Click += gcnew System::EventHandler(this, &MyForm::Button_calc_Click);
 			// 
-			// textBox1
+			// textBox_dist
 			// 
-			this->textBox1->Location = System::Drawing::Point(97, 25);
-			this->textBox1->Name = L"textBox1";
-			this->textBox1->Text = L"5";
-			this->textBox1->Size = System::Drawing::Size(100, 19);
-			this->textBox1->TabIndex = 1;
+			this->textBox_dist->Location = System::Drawing::Point(97, 25);
+			this->textBox_dist->Name = L"textBox_dist";
+			this->textBox_dist->Text = L"5";
+			this->textBox_dist->Size = System::Drawing::Size(100, 19);
+			this->textBox_dist->TabIndex = 1;
 			// 
-			// textBox2
+			// textBox_iso
 			// 
-			this->textBox2->Location = System::Drawing::Point(97, 62);
-			this->textBox2->Name = L"textBox2";
-			this->textBox2->Text = L"100";
-			this->textBox2->Size = System::Drawing::Size(100, 19);
-			this->textBox2->TabIndex = 2;
+			this->textBox_iso->Location = System::Drawing::Point(97, 62);
+			this->textBox_iso->Name = L"textBox_iso";
+			this->textBox_iso->Text = L"100";
+			this->textBox_iso->Size = System::Drawing::Size(100, 19);
+			this->textBox_iso->TabIndex = 2;
 			// 
-			// textBox3
+			// textBox_gn
 			// 
-			this->textBox3->Location = System::Drawing::Point(97, 202);
-			this->textBox3->Name = L"textBox3";
-			this->textBox3->Text = L"";
-			this->textBox3->Size = System::Drawing::Size(100, 19);
-			this->textBox3->TabIndex = 3;
-			this->textBox3->TextChanged += gcnew System::EventHandler(this, &MyForm::TextBox3_TextChanged);
+			this->textBox_gn->Location = System::Drawing::Point(97, 202);
+			this->textBox_gn->Name = L"textBox_gn";
+			this->textBox_gn->Text = L"";
+			this->textBox_gn->Size = System::Drawing::Size(100, 19);
+			this->textBox_gn->TabIndex = 3;
+			this->textBox_gn->TextChanged += gcnew System::EventHandler(this, &MyForm::TextBox_gn_TextChanged);
 			// 
 			// label1
 			// 
@@ -118,69 +118,69 @@ namespace cameraFlashCalculatorform {
 			this->label1->TabIndex = 4;
 			this->label1->Click += gcnew System::EventHandler(this, &MyForm::Label1_Click);
 			// 
-			// label2
+			// Label_dist
 			// 
-			this->label2->AutoSize = true;
-			this->label2->Location = System::Drawing::Point(43, 28);
-			this->label2->Name = L"label2";
-			this->label2->Size = System::Drawing::Size(50, 12);
-			this->label2->TabIndex = 5;
-			this->label2->Text = L"Distance";
-			this->label2->Click += gcnew System::EventHandler(this, &MyForm::Label2_Click);
+			this->label_dist->AutoSize = true;
+			this->label_dist->Location = System::Drawing::Point(43, 28);
+			this->label_dist->Name = L"Label_dist";
+			this->label_dist->Size = System::Drawing::Size(50, 12);
+			this->label_dist->TabIndex = 5;
+			this->label_dist->Text = L"Distance";
+			this->label_dist->Click += gcnew System::EventHandler(this, &MyForm::Label_dist_Click);
 			// 
-			// label3
+			// Label_iso
 			// 
-			this->label3->AutoSize = true;
-			this->label3->Location = System::Drawing::Point(58, 65);
-			this->label3->Name = L"label3";
-			this->label3->Size = System::Drawing::Size(23, 12);
-			this->label3->TabIndex = 6;
-			this->label3->Text = L"ISO";
-			this->label3->Click += gcnew System::EventHandler(this, &MyForm::Label3_Click);
+			this->label_iso->AutoSize = true;
+			this->label_iso->Location = System::Drawing::Point(58, 65);
+			this->label_iso->Name = L"Label_iso";
+			this->label_iso->Size = System::Drawing::Size(23, 12);
+			this->label_iso->TabIndex = 6;
+			this->label_iso->Text = L"ISO";
+			this->label_iso->Click += gcnew System::EventHandler(this, &MyForm::Label_iso_Click);
 			// 
-			// textBox4
+			// textBox_fVal
 			// 
-			this->textBox4->Location = System::Drawing::Point(97, 97);
-			this->textBox4->Name = L"textBox4";
-			this->textBox4->Text = L"4.0";
-			this->textBox4->Size = System::Drawing::Size(100, 19);
-			this->textBox4->TabIndex = 7;
+			this->textBox_fVal->Location = System::Drawing::Point(97, 97);
+			this->textBox_fVal->Name = L"textBox_fVal";
+			this->textBox_fVal->Text = L"4.0";
+			this->textBox_fVal->Size = System::Drawing::Size(100, 19);
+			this->textBox_fVal->TabIndex = 7;
 			// 
-			// label4
+			// Label_fVal
 			// 
-			this->label4->AutoSize = true;
-			this->label4->Location = System::Drawing::Point(47, 100);
-			this->label4->Name = L"label4";
-			this->label4->Size = System::Drawing::Size(43, 12);
-			this->label4->TabIndex = 8;
-			this->label4->Text = L"F value";
-			this->label4->Click += gcnew System::EventHandler(this, &MyForm::Label4_Click);
+			this->label_fVal->AutoSize = true;
+			this->label_fVal->Location = System::Drawing::Point(47, 100);
+			this->label_fVal->Name = L"Label_fVal";
+			this->label_fVal->Size = System::Drawing::Size(43, 12);
+			this->label_fVal->TabIndex = 8;
+			this->label_fVal->Text = L"F value";
+			this->label_fVal->Click += gcnew System::EventHandler(this, &MyForm::Label_fVal_Click);
 			// 
-			// label5
+			// label_gn
 			// 
-			this->label5->AutoSize = true;
-			this->label5->Location = System::Drawing::Point(58, 205);
-			this->label5->Name = L"label5";
-			this->label5->Size = System::Drawing::Size(21, 12);
-			this->label5->TabIndex = 9;
-			this->label5->Text = L"GN";
-			this->label5->Click += gcnew System::EventHandler(this, &MyForm::Label5_Click);
+			this->label_gn->AutoSize = true;
+			this->label_gn->Location = System::Drawing::Point(58, 205);
+			this->label_gn->Name = L"label_gn";
+			this->label_gn->Size = System::Drawing::Size(21, 12);
+			this->label_gn->TabIndex = 9;
+			this->label_gn->Text = L"GN";
+			this->label_gn->Click += gcnew System::EventHandler(this, &MyForm::Label_gn_Click);
 			// 
 			// MyForm
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 12);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
 			this->ClientSize = System::Drawing::Size(284, 261);
-			this->Controls->Add(this->label5);
-			this->Controls->Add(this->label4);
-			this->Controls->Add(this->textBox4);
-			this->Controls->Add(this->label3);
-			this->Controls->Add(this->label2);
+			this->Controls->Add(this->label_gn);
+			this->Controls->Add(this->label_fVal);
+			this->Controls->Add(this->textBox_fVal);
+			this->Controls->Add(this->label_iso);
+			this->Controls->Add(this->label_dist);
 			this->Controls->Add(this->label1);
-			this->Controls->Add(this->textBox3);
-			this->Controls->Add(this->textBox2);
-			this->Controls->Add(this->textBox1);
-			this->Controls->Add(this->button1);
+			this->Controls->Add(this->textBox_gn);
+			this->Controls->Add(this->textBox_iso);
+			this->Controls->Add(this->textBox_dist);
+			this->Controls->Add(this->button_calc);
 			this->Name = L"MyForm";
 			this->Load += gcnew System::EventHandler(this, &MyForm::MyForm_Load);
 			this->ResumeLayout(false);
@@ -188,34 +188,33 @@ namespace cameraFlashCalculatorform {
 
 		}
 #pragma endregion
-	private: System::Void Button1_Click(System::Object^ sender, System::EventArgs^ e) {
+	private: System::Void Button_calc_Click(System::Object^ sender, System::EventArgs^ e) {
 		//int hoge = 334;
 		//String^ myString1 = hoge.ToString();
-		//this->textBox1->Text = myString1;
+		//this->textBox_dist->Text = myString1;
 
-
-		double dist = double::Parse(this->textBox1->Text );
-		double iso  = double::Parse(this->textBox2->Text);
-		double fVal = double::Parse(this->textBox4->Text);
-		//double dist = std::stod(this->textBox1);
+		double dist = double::Parse(this->textBox_dist->Text );
+		double iso  = double::Parse(this->textBox_iso->Text);
+		double fVal = double::Parse(this->textBox_fVal->Text);
+		//double dist = std::stod(this->textBox_dist);
 		
 		double gn = fVal * dist * sqrt(100/iso);
 		
-		this->textBox3->Text = gn.ToString();
+		this->textBox_gn->Text = gn.ToString();
 	}
 	private: System::Void Label1_Click(System::Object^ sender, System::EventArgs^ e) {
 	}
 private: System::Void MyForm_Load(System::Object^ sender, System::EventArgs^ e) {
 }
-private: System::Void Label2_Click(System::Object^ sender, System::EventArgs^ e) {
+private: System::Void Label_dist_Click(System::Object^ sender, System::EventArgs^ e) {
 }
-private: System::Void Label3_Click(System::Object^ sender, System::EventArgs^ e) {
+private: System::Void Label_iso_Click(System::Object^ sender, System::EventArgs^ e) {
 }
-private: System::Void Label4_Click(System::Object^ sender, System::EventArgs^ e) {
+private: System::Void Label_fVal_Click(System::Object^ sender, System::EventArgs^ e) {
 }
-private: System::Void Label5_Click(System::Object^ sender, System::EventArgs^ e) {
+private: System::Void Label_gn_Click(System::Object^ sender, System::EventArgs^ e) {
 }
-private: System::Void TextBox3_TextChanged(System::Object^ sender, System::EventArgs^ e) {
+private: System::Void TextBox_gn_TextChanged(System::Object^ sender, System::EventArgs^ e) {
 }
 };
 }
